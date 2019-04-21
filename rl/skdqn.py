b7707485a6c87386bc7ad2b50d3222c4eb973901
mattvlaw <mattvlaw@gmail.com>
2019-04-21 12:29:26 -0400

    added argparse and guard to allow argument for loading saved models

--- rl/skdqn.py
+++ rl/skdqn.py
@@ -5,20 +5,35 @@ from rl.environment import Environment
 from rl.qagent import QAgent
 from bayesopt.bo import SKOutcomes, PreferenceDummy, SKBayesOpt
 import time
+import argparse
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("save_name", nargs='?', default='empty')
+    args = parser.parse_args()
+    if args.save_name == 'empty':
+        saved_model = None
+        saved_weights = None
+    else:
+        base = args.save_name
+        print("Using save name {}".format(base))
+        saved_model = base + ".yaml"
+        saved_weights = base + ".h5"
     outcomes = SKOutcomes()
     pd = PreferenceDummy(outcomes)
     bo = SKBayesOpt(pd)
     env = Environment(width = 65, height = 34, tables = [], equipment = [], staff = [], reward_model = bo)
-    #qa = QAgent(input_shape = (65,34),saved_model="skdqn.yaml", saved_weights="skdqn.h5")
-    qa = QAgent(input_shape = (65,34))
+    qa = QAgent(input_shape = (65,34),saved_model=saved_model, saved_weights=saved_weights)
+    #qa = QAgent(input_shape = (65,34))
     episodes = 10
     base_iterations_per_ep = 2 
     max_iterations_per_ep = 100
     restaurants = []
     state = env.reset(init_state = None) # implement this!!!
+    stats = []
     for e in range(episodes):
+        episode_stats = {"mistakes":0,"nops":0,"actions":0,"rewards":[]}
         print("Episode {}".format(e))
         state = env.reset(init_state = None)
         restaurants.append((state,None,0))
@@ -29,7 +44,15 @@ if __name__ == "__main__":
             next_state, reward = env.step(action)
             restaurants.append((next_state,action,reward))
             qa.remember(state.image, action, reward, next_state.image)
+            if reward == -1e6:
+                episode_stats["mistakes"] += 1
+            elif reward == -1e8:
+                episode_stats["nops"] += 1
+            else:
+                episode_stats["actions"] += 1
+            episode_stats["rewards"].append(reward)
         next_state.png.show()
+        stats.append(episode_stats)
         qa.retrain(num_iter)
     qa.save_model("skdqn")
 
